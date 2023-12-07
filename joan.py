d18f9f2bb1bd293900b2db5ebeff1d0bd222ee62
joansunset <153150292+joansunset@users.noreply.github.com>
2023-12-07 10:33:04 +0800

    Add files via upload

---
+++ joan.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# Find more emoji's here: https://www.webfx.com/tools/emoji_cheat_sheet/
+st.set_page_config(page_title="Joanna's Weblog", page_icon=":tada:", layout="wide")
+
+# ---- Header ----
+with st.container():
+    st.title("Hi, I am Joanna Mae E. Ponio")
+    st.write("19 years old.")
+    st.write("Student in Surigao del Norte State University with a program Bachelor of Science in Computer Engineering.")
+    st.write("And I lived in Brgy. Punod Purok-1, Southern Leyte.")
+    st.write("My hobbies are Taking pictures of Sunsets & Sleeping.")
+    st.write("If you want to know me more just click my social account.")
+    st.write("[Learn More >](https://www.facebook.com/joannamae.ponio.5)")
